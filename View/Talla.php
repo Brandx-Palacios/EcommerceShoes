f53a43872f02697f3ef9f44cc9ec266d87bfee79
NitroZeus774 <nitrozeusl774@gmail.com>
2024-09-24 17:13:47 -0500

    added styles for Talla.php

--- View/Talla.php
+++ View/Talla.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type = "text/css" href = "./css/talla_style_form.css">
     <title>Registrar Producto</title>
 </head>
 
@@ -26,58 +27,62 @@
         $colors = $controllerProducts->GetAllColors();
         $Sizes = $controllerProducts->GetAllSize();
     ?>
-    <h2>PARA LA TABLA PRODUCTO</h2>
-    <form action="Uo.php" method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+    
+    <span id = "circle"></span>
+    <span id = "circle2"></span>
+    <div class = "cont-t-prod">
+        
+        <h2>PARA LA TABLA PRODUCTO</h2>
+        <form action="Uo.php" method="POST" enctype="multipart/form-data">
+            <div class = "cont-in-prod">
+                <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>" class = "in-form">
 
-        <input type="text" name="NOMBRE" placeholder="NOMBRE" required>
-        <!-- Mostrar el ID del último producto aquí -->
+                <input type="text" name="NOMBRE" placeholder="NOMBRE" required class = "in-form">
+                <!-- Mostrar el ID del último producto aquí -->
 
-        <input type="text" name="DESCRIPCION" placeholder="DESCRIPCION" required>
-        <input type="text" name="SKU" placeholder="SKU" required>
-        <input type="number" name="PRECIO" placeholder="PRECIO" required>
-        <input type="number" name="DESCUENTO" placeholder="DESCUENTO" required>
+                <input type="text" name="DESCRIPCION" placeholder="DESCRIPCION" required class = "in-form">
+                <input type="text" name="SKU" placeholder="SKU" required class = "in-form">
+                <input type="number" name="PRECIO" placeholder="PRECIO" required class = "in-form">
+                <input type="number" name="DESCUENTO" placeholder="DESCUENTO" required class = "in-form">
 
-        <select name="idCategoria" required>
-            <?php foreach ($category as $categoria) { ?>
-            <option value="<?php echo htmlspecialchars($categoria['ID']); ?>">
-                <?php echo htmlspecialchars($categoria['NOMBRE']); ?></option>
-            <?php } ?>
-        </select>
+                <select name="idCategoria" required>
+                    <?php foreach ($category as $categoria) { ?>
+                    <option value="<?php echo htmlspecialchars($categoria['ID']); ?>">
+                        <?php echo htmlspecialchars($categoria['NOMBRE']); ?></option>
+                    <?php } ?>
+                </select>
 
-        <input type="hidden" name="accionesProductos" value="RegistrarProducto">
-        <input type="submit" value="Registrar Producto">
-    </form>
+                <input type="hidden" name="accionesProductos" value="RegistrarProducto" class = "in-form">
+                <input type="submit" value="Registrar Producto" class = "in-form" id = "subm-btn">
+                
+            </div>
+        </form>
+    </div>
 
 
-
-
-
-
+    <div class="img-t-cont">
     <h2>PARA LA TABLA IMÁGENES</h2>
-    <form action="Uo.php" method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+        <form action="Uo.php" method="POST" enctype="multipart/form-data">
+            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
 
-        <!-- Campo para seleccionar el color asociado -->
-        <select id="colorSelectImages" name="COLOR" required style="width: 100px; height: 40px;">
-            <?php foreach ($colors as $color) { ?>
-            <option value="<?php echo htmlspecialchars($color['ID']); ?>"
-                style="background-color: <?php echo htmlspecialchars($color['COLOR']); ?>; color: <?php echo htmlspecialchars($color['COLOR']); ?>;">
-                <?php echo htmlspecialchars($color['COLOR']); ?>
-            </option>
-            <?php } ?>
-        </select>
+            <!-- Campo para seleccionar el color asociado -->
+            <select id="colorSelectImages" name="COLOR" required style="width: 100px; height: 40px;">
+                <?php foreach ($colors as $color) { ?>
+                <option value="<?php echo htmlspecialchars($color['ID']); ?>"
+                    style="background-color: <?php echo htmlspecialchars($color['COLOR']); ?>; color: <?php echo htmlspecialchars($color['COLOR']); ?>;">
+                    <?php echo htmlspecialchars($color['COLOR']); ?>
+                </option>
+                <?php } ?>
+            </select>
 
+        
+            <!-- Subir múltiples imágenes -->
+            <input type="file" name="fotos[]" accept="image/*" multiple required>
 
-        <!-- Subir múltiples imágenes -->
-        <input type="file" name="fotos[]" accept="image/*" multiple required>
-
-        <input type="hidden" name="accionesImagenes" value="RegistrarImagenes">
-        <input type="submit" value="Registrar Imágenes">
-    </form>
-
-
-
+            <input type="hidden" name="accionesImagenes" value="RegistrarImagenes">
+            <input type="submit" value="Registrar Imágenes">
+        </form>
+    </div>
 
 
 
@@ -90,59 +95,61 @@
 
 
 
+    <div class="col-t-cont">
+        <!-- Formulario para registrar color -->
+        <h2>PARA LA TABLA COLOR</h2>
+        <form action="Uo.php" method="POST">
+            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+            <input type="color" id="color" name="COLOR" required>
+            <input type="hidden" name="accionesColor" value="RegistrarColor">
+            <input type="submit" value="Registrar Color">
+        </form>
+    </div>
 
-    <!-- Formulario para registrar color -->
-    <h2>PARA LA TABLA COLOR</h2>
-    <form action="Uo.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
-        <input type="color" id="color" name="COLOR" required>
-        <input type="hidden" name="accionesColor" value="RegistrarColor">
-        <input type="submit" value="Registrar Color">
-    </form>
 
+    <div class="talla-t-cont">
+        <h2>PARA LA TABLA TALLA</h2>
 
+        <form action="Uo.php" method="POST">
+            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+            <input type="text" name="TALLA" placeholder="Talla" required>
+            <input type="hidden" name="accionesTalla" value="RegistrarTalla">
+            <input type="submit" value="Registrar Talla">
+        </form>
+    </div>
 
+    <div class="regst-stk-cont">
+        <h2>Registrar Stock</h2>
+        <form action="Uo.php" method="POST">
+            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
 
-    <h2>PARA LA TABLA TALLA</h2>
+            <select name="idTalla" required>
+                <?php foreach ($Sizes as $size) { ?>
+                <option value="<?php echo htmlspecialchars($size['ID']); ?>">
+                    <?php echo htmlspecialchars($size['TALLA']); ?></option>
+                <?php } ?>
+            </select>
 
-    <form action="Uo.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
-        <input type="text" name="TALLA" placeholder="Talla" required>
-        <input type="hidden" name="accionesTalla" value="RegistrarTalla">
-        <input type="submit" value="Registrar Talla">
-    </form>
+            <!-- Selección del color -->
+            <select id="colorSelectStock" name="COLOR" required>
+                <?php foreach ($colors as $color) { ?>
+                <option value="<?php echo htmlspecialchars($color['ID']); ?>">
+                    <?php echo htmlspecialchars($color['COLOR']); ?></option>
+                <?php } ?>
+            </select>
 
-    <h2>Registrar Stock</h2>
-    <form action="Uo.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+            <input type="text" name="CANTIDAD" placeholder="CANTIDAD" required>
+            <input type="hidden" name="accionesStock" value="RegistrarStock">
+            <input type="submit" value="Registrar Stock">
+        </form>
+        <form action="Uo.php" method="POST">
+            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+            <input type="hidden" name="guardarCambiosProducto" value="guardarCambios">
+            <input type="submit" value="Guardar Cambios">
+        </form>
 
-        <select name="idTalla" required>
-            <?php foreach ($Sizes as $size) { ?>
-            <option value="<?php echo htmlspecialchars($size['ID']); ?>">
-                <?php echo htmlspecialchars($size['TALLA']); ?></option>
-            <?php } ?>
-        </select>
-
-        <!-- Selección del color -->
-        <select id="colorSelectStock" name="COLOR" required>
-            <?php foreach ($colors as $color) { ?>
-            <option value="<?php echo htmlspecialchars($color['ID']); ?>">
-                <?php echo htmlspecialchars($color['COLOR']); ?></option>
-            <?php } ?>
-        </select>
-
-        <input type="text" name="CANTIDAD" placeholder="CANTIDAD" required>
-        <input type="hidden" name="accionesStock" value="RegistrarStock">
-        <input type="submit" value="Registrar Stock">
-    </form>
-    <form action="Uo.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
-        <input type="hidden" name="guardarCambiosProducto" value="guardarCambios">
-        <input type="submit" value="Guardar Cambios">
-    </form>
-
-    <a href="ProductsAll.php">VER TODO</a>
-
+        <a href="ProductsAll.php">VER TODO</a>
+    </div>
 
 
     <script>
